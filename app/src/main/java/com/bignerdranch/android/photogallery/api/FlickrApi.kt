70575f0555fd788ec9750fdb59f4e9fa82288a08
Sanders <sndrs90@gmail.com>
2022-10-19 14:58:12 +0300

    Get JSON response from Flickr. Add GalleryItem

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -5,6 +5,12 @@ import retrofit2.http.GET
 
 interface FlickrApi {
 
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=be7dc1096339c67be41178440284e4ec" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
 }
